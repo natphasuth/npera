a0596521e41533635efcd141a2668397fcc97793
root <root@docker.hq.npera.co.th>
2017-11-01 23:59:36 +0700

    fourth commit

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World2\n');
 }).listen(9080, 'localhost');
 console.log('Server running at http://localhost:9080/');
